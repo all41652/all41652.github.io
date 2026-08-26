fcb69f631205711913a4c33d769dc5b6f77cfc3f
all41652 <all41652@students.apsb.org>
2026-08-26 19:25:36 +0000

    platformer

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -3,7 +3,6 @@ $(function () {
   canvas = document.getElementById("canvas");
   ctx = canvas.getContext("2d");
   window.addEventListener("load", loadJson);
-
   function setup() {
     if (firstTimeSetup) {
       halleImage = document.getElementById("player");
@@ -27,17 +26,32 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
+    toggleGrid();
 
 
     // TODO 2 - Create Platforms
-
-
-
+createPlatform(0, 720, 200, 20, "hotpink");
+createPlatform(250, 600, 250, 20, "hotpink");
+createPlatform(250, 360, 100, 50, "hotpink");
+createPlatform(1150, 400, 25, 270, "hotpink"); 
+ createPlatform(900, 135, 300, 10, "red");
+createPlatform(450, 470, 50, 10, "hotpink");
+createPlatform(400, 250, 150, 20, "red");
+createPlatform(1350, 400, 50, 50, "red");
+createCannon("top", 200, 1000)
+createCannon("right", 300, 1000);
+createBadPlatform(200, 725, 1400, 15);
+createCannon("right", 115, 4000);
+createPlatform(900, 650, 250, 20, "hotpink");
+createPlatform(720, 600, 50, 20, "hotpink");
+createPlatform(1200, 0, 15, 145, "red");
+createPlatform(600, 150, 100, 15,"red");
+createCannon("top", 400, 3000);
 
     // TODO 3 - Create Collectables
-
-
+createCollectable("steve", 1350, 50);
+createCollectable("database", 1150, 100, 0.7, 0.7);
+createCollectable("database", 1100, 600, 0.5, 0.5);
 
     
     // TODO 4 - Create Cannons
