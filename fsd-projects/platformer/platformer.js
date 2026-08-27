05a2c3311dbe329d505c50d34c68e7bc23dfc328
all41652 <all41652@students.apsb.org>
2026-08-27 19:01:58 +0000

    platformer

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -17,7 +17,7 @@ $(function () {
 
     // Create walls - do not delete or modify this code
     createPlatform(-50, -50, canvas.width + 100, 50); // top wall
-    createPlatform(-50, canvas.height - 10, canvas.width + 100, 200, "rgb(118, 0, 233)"); // bottom wall
+    createPlatform(-50, canvas.height - 10, canvas.width + 100, 200, "rgb(255, 254, 254)"); // bottom wall
     createPlatform(-50, -50, 50, canvas.height + 500); // left wall
     createPlatform(canvas.width, -50, 50, canvas.height + 100); // right wall
 
@@ -30,29 +30,30 @@ $(function () {
 
 
     // TODO 2 - Create Platforms
-createPlatform(0, 720, 200, 20, "hotpink");
-createPlatform(250, 600, 250, 20, "hotpink");
-createPlatform(250, 360, 100, 50, "hotpink");
-createPlatform(1150, 400, 25, 270, "hotpink"); 
- createPlatform(900, 135, 300, 10, "red");
-createPlatform(450, 470, 50, 10, "hotpink");
-createPlatform(400, 250, 150, 20, "red");
-createPlatform(1350, 400, 50, 50, "red");
-createCannon("top", 200, 1000)
-createCannon("right", 300, 1000);
+createPlatform(0, 720, 200, 20, "white");
+createPlatform(250, 600, 250, 20, "white");
+createPlatform(250, 360, 100, 20, "white");
+createPlatform(1150, 400, 25, 270, "white"); 
+ createPlatform(900, 150, 300, 10, "white");
+createPlatform(450, 470, 50, 10, "white");
+createPlatform(400, 250, 150, 20, "white");
+createPlatform(1350, 400, 50, 50, "white");
+createCannon("top", 200, 3000)
+createCannon("right", 300, 3000);
 createBadPlatform(200, 725, 1400, 15);
-createCannon("right", 115, 4000);
-createPlatform(900, 650, 250, 20, "hotpink");
-createPlatform(720, 600, 50, 20, "hotpink");
-createPlatform(1200, 0, 15, 145, "red");
-createPlatform(600, 150, 100, 15,"red");
-createCannon("top", 400, 3000);
-
+createCannon("right", 200, 9000);
+createPlatform(900, 650, 250, 20, "white");
+createPlatform(720, 600, 50, 20, "white");
+createPlatform(1200, 10, 15, 150, "white");
+createPlatform(600, 190, 100, 15,"white");
+createCannon("top", 500, 3000);
+createPlatform(100, 200, 100, 20,"white");
+createCannon("right", 600, 3000);
     // TODO 3 - Create Collectables
-createCollectable("steve", 1350, 50);
+createCollectable("steve", 100, 700);
 createCollectable("database", 1150, 100, 0.7, 0.7);
 createCollectable("database", 1100, 600, 0.5, 0.5);
-
+createCollectable("steve", 150, 150);
     
     // TODO 4 - Create Cannons
 
